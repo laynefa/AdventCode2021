558b4f4403805d66ce6e4207c2d6135ea96d534d
Layne <layne.j.fabian@gmail.com>
2021-12-03 23:42:30 -0500

    removing second nested loop

--- Day3/Puzzle.py
+++ Day3/Puzzle.py
@@ -1,34 +1,35 @@
 def part_one():
     binary_data = []
     with open('.//Day3//input.txt', 'r') as binary_fp:
-        for line in binary_fp:
-            bits = []
-            for bit in line.strip():
-                bits.append(bit)
-            binary_data.append(bits)
-    #access by binary_data[0,0] then [1,0], [2,0], etc
-    count0 = 0
-    count1 = 0
-    gamma_rate = ''
-    epsilon_rate = ''
-    for column in range(0, len(binary_data[0])): #columns
-        for row in range(0, len(binary_data)): #rows
-            bit = binary_data[row][column]
-            if bit == '0':
-                count0 += 1
-            elif bit == '1':
-                count1 += 1
+        first_line = binary_fp.readline().strip()
+        counts0 = []
+        counts1 = []
+        size = len(first_line)
+        for i in range(size):
+            if first_line[i] == '0':
+                counts0.append(1)
+                counts1.append(0)
             else:
-                raise ValueError('Invalid bit value')
-        if count0 > count1:
-            gamma_rate += '0'
-            epsilon_rate += '1'
-        else:
-            gamma_rate += '1'
-            epsilon_rate += '0'
-        count0 = 0
-        count1 = 0
+                counts1.append(1)
+                counts0.append(0)
+        for line in binary_fp:
+            line = line.strip()
+            for bit_count, bit in enumerate(line):
+                if bit == '0':
+                    counts0[bit_count] += 1
+                else:
+                    counts1[bit_count] += 1
+        gamma_rate = ''
+        epsilon_rate = ''
+        for i in range(size):
+            if counts0[i] > counts1[i]:
+                gamma_rate += '0'
+                epsilon_rate += '1'
+            else:
+                gamma_rate += '1'
+                epsilon_rate += '0'
     print('gamma rate: ' + gamma_rate)
     print('epsilon rate: ' + epsilon_rate)
     print('power consumption: ' + str(int(gamma_rate, 2) * int(epsilon_rate, 2)))
+    
 part_one()
