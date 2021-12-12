8df89690c0a3b243d3b2218afd66889c5bbc57e9
Layne <layne.j.fabian@gmail.com>
2021-12-11 23:52:58 -0500

    completed day 4

--- Day4/Puzzle.py
+++ Day4/Puzzle.py
@@ -5,6 +5,7 @@ class Board():
         self.locations = {}
         self.row_count = [0,0,0,0,0]
         self.column_count = [0,0,0,0,0]
+        self.won = False
         for row_num, line in enumerate(board_contents):
             row = []
             for col_num, i in enumerate(range(0, 14, 3)):
@@ -15,12 +16,13 @@ class Board():
             self.bingo_board.append(row)
             
     def mark_number(self, number):
-        if number in self.locations:
+        if not self.won and number in self.locations:
             self.sum -= number
             row,col = self.locations[number]
             row_marks = self.row_count[row]
             col_marks = self.column_count[col]
             if row_marks == 4 or col_marks == 4:
+                self.won = True
                 return True
             self.row_count[row] = row_marks + 1
             self.column_count[col] = col_marks + 1
@@ -30,15 +32,28 @@ class Board():
 
     def print_score(self, number):
         print(self.sum * number)
-
+                
 def main(numbers, boards):
+    last_board = 0
+    boards_won = 0
+    total_boards = len(boards)
+    number = None
     for number in numbers:
-        for board in boards:
+        for count, board in enumerate(boards):
             if board.mark_number(number):
-                print('current board wins')
-                board.print_board()
-                board.print_score(number)
-                return
+                if boards_won == 0:
+                    print('Board #' + str(count+1) + ' is the first to win')
+                    board.print_board()
+                    board.print_score(number)
+                boards_won += 1
+                if boards_won == total_boards:
+                    last_board = count
+                    break
+        if boards_won == total_boards:
+            break
+    print('last board to win is board #' + str(last_board+1))
+    boards[last_board].print_board()
+    boards[last_board].print_score(number)
 
 if __name__ == '__main__':
     with open('./Day4/numbers.txt', 'r') as numbers_file, open('./Day4/boards.txt') as boards_file:
